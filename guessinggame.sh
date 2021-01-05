e7d488f5a77902fa3fe8c76c48af49a5f5fa1d7c
kellyduarte <72663055+kellyduarte@users.noreply.github.com>
2021-01-04 21:16:11 -0300

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,28 @@
 echo "[Welcome to Guessing Game]"
+
 function ask_guess {
-echo "Please enter the number of files in the currenct directory:"
-read guess
-files=$(ls -1 | wc -l)
+    echo "Please enter the number of files in the currenct directory:"
+    read guess
+  files=$(ls -1 | wc -l)
+}
+
+ask_guess
 
 while [[ $guess -ne $files ]]
 do
-if [[ $guess -lt $files ]]
-then
-echo "Sorry, too low. Please, try again."
-echo "Please enter the number of files in the currenct directory:"
-read guess
-else
-echo "Sorry, too high. Please, try again."
-echo "Please enter the number of files in the currenct directory:"
-read guess
-fi
-ask_guess
-done
-echo "Congratulations! Your answer is correct!"
+  if [[ $guess -lt $files ]]
+  then
+  echo "Sorry, too low. Please, try again."
+  echo "Please enter the number of files in the currenct directory:"
+  read guess
+  else
+  echo "Sorry, too high. Please, try again."
+  echo "Please enter the number of files in the currenct directory:"
+  read guess
+  fi
+  ask_guess
+  done
+  echo "Congratulations! Your answer is correct!"
 }
 
 
